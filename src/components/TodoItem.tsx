f22bb5e02a88876696d317b8a83308afc5e55c5e
Satomi Watanabe <satomi@hinata.space>
2023-12-29 18:37:51 +0900

    トップ画面に表示するタスクのコンポーネントを作成

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,31 @@
+import { TodoInfo } from "@/util/types";
+import Dropdown from "./Dropdown";
+import { timestampConvertToFormatDate } from "@/util/tools";
+
+interface Props {
+  todoInfo: TodoInfo;
+  index: number;
+}
+
+const TodoItem = ({ todoInfo, index }: Props) => {
+  return (
+    <div className="flex my-2 items-center w-full bg-slate-400">
+      {/* ステータス */}
+      <Dropdown
+        index={index}
+        initialStatus={todoInfo.status}
+        onSelect={(status) => console.log(status)}
+      />
+
+      {/* タイトル */}
+      <div className="mx-2 flex-grow">{todoInfo.title}</div>
+
+      {/* 完了予定日 */}
+      <div className="mx-2">
+        {timestampConvertToFormatDate(todoInfo.endDate)}
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
