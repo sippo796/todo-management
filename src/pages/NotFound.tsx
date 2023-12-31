c88e3d764d60777d08ab16cbe9cf083c5988a1ad
Satomi Watanabe <satomi@hinata.space>
2023-12-31 16:25:45 +0900

    NotFoundページの実装

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import PageBase from "@/components/PageBase";
+
+const NotFound = () => {
+  return (
+    <PageBase>
+      <h1>お探しのページは見つかりません。</h1>
+    </PageBase>
+  );
+};
+
+export default NotFound;
