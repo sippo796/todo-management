5363632ec74650133563619da558b75f80b9e425
Satomi Watanabe <satomi@hinata.space>
2023-12-31 16:27:50 +0900

    タスク新規作成、更新ページを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { RoutePath } from "@/util/defines";
 import Top from "@/pages/Top";
-import NotFound from "./pages/NotFound";
+import NotFound from "@/pages/NotFound";
+import Create from "@/pages/Create";
+import Update from "@/pages/Update";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
         {/* Top */}
         <Route path={RoutePath.Top} element={<Top />} />
         {/* 新規登録 */}
+        <Route path={RoutePath.Create} element={<Create />} />
         {/* 編集 */}
+        <Route path={RoutePath.Update} element={<Update />} />
         {/* NotFound */}
         <Route path={RoutePath.NotFound} element={<NotFound />} />
       </Routes>
---
+++ src/pages/Create.tsx
@@ -0,0 +1,7 @@
+import PageBase from "@/components/PageBase";
+
+const Create = () => {
+  return <PageBase>Create</PageBase>;
+};
+
+export default Create;
