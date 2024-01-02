f4a2ffab7a464baa113622046d3966ad3a410b1d
Satomi Watanabe <satomi@hinata.space>
2024-01-02 14:06:37 +0900

    リセット機能を外だし

--- src/test/mock.test.ts
+++ src/test/mock.test.ts
@@ -7,18 +7,22 @@ import {
   readData,
 } from "../util/tools";
 
-describe("モックサーバー", () => {
-  beforeAll(async () => {
-    // db.jsonのリセット
-    const response = await fetch(`http://localhost:3000/todos`);
-    const todos = await response.json();
+export const resetDatabase = async () => {
+  // db.jsonのリセット
+  const response = await fetch(`http://localhost:3000/todos`);
+  const todos = await response.json();
 
-    // 各todoを削除
-    await Promise.all(
-      todos.map((todo: TodoInfo) =>
-        fetch(`http://localhost:3000/todos/${todo.id}`, { method: "DELETE" })
-      )
-    );
+  // 各todoを削除
+  await Promise.all(
+    todos.map((todo: TodoInfo) =>
+      fetch(`http://localhost:3000/todos/${todo.id}`, { method: "DELETE" })
+    )
+  );
+};
+
+describe("モックサーバー正常系テスト", () => {
+  beforeAll(async () => {
+    await resetDatabase();
   });
 
   it("全データの取得:0件であること", async () => {
@@ -64,11 +68,45 @@ describe("モックサーバー", () => {
     const result = await readData(1);
     expect(result).toEqual(update);
   });
+});
 
-  it("データの削除", async () => {
-    await deleteData(1);
+describe("モックサーバー正常系テスト（対象なし）", () => {
+  beforeAll(async () => {
+    await resetDatabase();
+
+    await addData({
+      id: 1,
+      title: "test1",
+      detail: "test1の詳細",
+      status: 0,
+      endDate: 12345,
+    });
+  });
+
+  it("データの取得（対象なし）", async () => {
+    const todoInfo = await readData(10);
+
+    expect(todoInfo).toEqual({});
+  });
+
+  it("データの更新（対象なし）", async () => {
+    const update: TodoInfo = {
+      id: 10,
+      title: "test1-update",
+      detail: "test1の詳細を更新",
+      status: 0,
+      endDate: 12345,
+    };
+    await updateData(update);
+
+    const result = await readData(10);
+    expect(result).toEqual({});
+  });
+
+  it("データの削除（対象なし）", async () => {
+    await deleteData(10);
 
     const allData = await readAll();
-    expect(allData.length).toBe(0);
+    expect(allData.length).toBe(1);
   });
 });
