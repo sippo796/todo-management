80574ad7da228fb336537228bd4082ff6ff712ee
Satomi Watanabe <satomi@hinata.space>
2024-01-05 12:04:28 +0900

    @applyが認識できなかったので、定義の仕方を変更した

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -2,7 +2,6 @@ import { SortType } from "@/util/types";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import { useState } from "react";
-import "@/styles/sort.css";
 
 interface Props {
   onSort: (type: SortType, sortAscending: boolean) => void;
@@ -13,12 +12,15 @@ const Sort = ({ onSort }: Props) => {
   const [titleSortAscending, setTitleSortAscending] = useState(true);
   const [dateSortAscending, setDateSortAscending] = useState(true);
 
+  const sortButtonStyle =
+    "px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700";
+
   return (
     <div>
       <div className="w-full flex shadow-sm mt-2 px-2" role="group">
         <button
           type="button"
-          className="w-32 sort-button"
+          className={`w-32 ${sortButtonStyle}`}
           onClick={() => {
             onSort(SortType.status, !statusSortAscending);
             setStatusSortAscending((prev) => !prev);
@@ -29,7 +31,7 @@ const Sort = ({ onSort }: Props) => {
         </button>
         <button
           type="button"
-          className="flex-grow sort-button"
+          className={`flex-grow ${sortButtonStyle}`}
           onClick={() => {
             onSort(SortType.title, !titleSortAscending);
             setTitleSortAscending((prev) => !prev);
@@ -40,7 +42,7 @@ const Sort = ({ onSort }: Props) => {
         </button>
         <button
           type="button"
-          className="sort-button"
+          className={`${sortButtonStyle}`}
           onClick={() => {
             onSort(SortType.date, !dateSortAscending);
             setDateSortAscending((prev) => !prev);
