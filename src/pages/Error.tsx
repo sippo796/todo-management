cf644f4e7c192f055b16558dc53e7ea1e96d6106
Satomi Watanabe <satomi@hinata.space>
2024-01-08 13:09:58 +0900

    エラー画面の修正

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -2,11 +2,12 @@ import PageBase from "@/components/PageBase";
 import { FallbackProps } from "react-error-boundary";
 
 const Error = ({ error }: FallbackProps) => {
+  console.log(`error = ${error}`);
   return (
     <PageBase>
       <div>
-        <h1>Error</h1>
-        <p>{error.message}</p>
+        <h2>Error</h2>
+        <p>{error}</p>
       </div>
     </PageBase>
   );
