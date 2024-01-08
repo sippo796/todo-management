3918a278b5104b8bd14504897ae67b2d5064903e
Satomi Watanabe <satomi@hinata.space>
2024-01-08 13:44:12 +0900

    ステータスによって色を変更する処理を追加

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,20 +1,36 @@
-import { TodoInfo } from "@/util/types";
+import { StatusDef, TodoInfo } from "@/util/types";
 import Dropdown from "./Dropdown";
-import { timestampConvertToFormatDate } from "@/util/tools";
+import { timestampConvertToFormatDate, updateData } from "@/util/tools";
 import { useNavigate } from "react-router-dom";
 import { RoutePath } from "@/util/defines";
+import { useEffect, useState } from "react";
 
 interface Props {
   todoInfo: TodoInfo;
   index: number;
+  onError: (message: string) => void;
 }
 
-const TodoItem = ({ todoInfo, index }: Props) => {
+const TodoItem = ({ todoInfo, index, onError }: Props) => {
+  const getColor = (status: StatusDef) => {
+    if (status === StatusDef.completed) {
+      return "bg-gray-500";
+    } else if (status === StatusDef.inProgress) {
+      return "bg-green-200";
+    } else {
+      return "bg-red-100";
+    }
+  };
   const navigate = useNavigate();
+  const [color, setColor] = useState(getColor(todoInfo.status));
+
+  useEffect(() => {
+    setColor(getColor(todoInfo.status));
+  }, [todoInfo.status]);
 
   return (
     <div
-      className="flex my-2 items-center w-full bg-slate-400"
+      className={`flex my-2 items-center w-full ${color}`}
       data-testid={`todoinfo`}
       onDoubleClick={() => {
         navigate(RoutePath.Edit, {
